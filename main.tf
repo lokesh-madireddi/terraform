101a580edf7082dd8ff367b22ce95c576feb42f0
Lokesh Madireddi <lokeshmadireddi2000@gmail.com>
2025-12-31 01:35:00 +0530

    Modify EKS module settings and add public access
    Updated EKS configuration with new node group settings and added public endpoint access.

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-#################################
+#######################################
 # VPC
-#################################
+########################################
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.5.1"
@@ -21,9 +21,9 @@ module "vpc" {
   }
 }
 
-#################################
-# EKS
-#################################
+########################################
+# EKS Cluster
+########################################
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
@@ -34,12 +34,19 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  # 🔑 IMPORTANT: Enable public endpoint access
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
+
+  # EKS Managed Node Group
   eks_managed_node_groups = {
     default = {
-      desired_size   = 2
+      name           = "default-node-group"
+      desired_size   = 1
       min_size       = 1
-      max_size       = 3
+      max_size       = 2
       instance_types = [var.node_instance_type]
+      capacity_type  = "ON_DEMAND"
     }
   }
 
